365f9fc52cb84ef6cd900f7fec995ad612d3588f
ErmalBajraktaari <103382617+ErmalBajraktaari@users.noreply.github.com>
2022-04-10 20:47:05 +0200

    Add files via upload

---
+++ admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .import models
+# Register your models here.
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('title','status', 'slug', 'author',)
+
+admin.site.register(models.Post, AuthorAdmin)
+
+
